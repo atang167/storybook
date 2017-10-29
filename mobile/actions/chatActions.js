4d29d235eb3ee1ed73638f03eeaa1026a6c5785b
Angie Tang <atang167@gmail.com>
2017-10-29 14:26:02 -0700

    Add room list to store

--- mobile/actions/chatActions.js
+++ mobile/actions/chatActions.js
@@ -9,3 +9,15 @@ export const exitRoom = (roomId) => {
     dispatch({type: 'EXIT_ROOM', payload: roomId});
   }
 }
+
+export const getRooms = (userId) => {
+  return function(dispatch) {
+    axios.get('/api/chat/' + userId)
+    .then(({ data }) => {
+      dispatch({type: 'GET_ALL_ROOMS', payload: data});
+    })
+    .catch(err => {
+      dispatch({type: 'ROOM_LIST_FAIL', payload: err})
+    })
+  }
+}
